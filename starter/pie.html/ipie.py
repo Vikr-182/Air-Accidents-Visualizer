55c64989ac0f6d452ce803dfd1b4f06176d5a039
bhavyajeet <bhavyajeet29@gmail.com>
2020-04-30 14:12:46 +0530

    scri[t done

--- starter/pie.html/ipie.py
+++ starter/pie.html/ipie.py
@@ -1,5 +1,5 @@
 import json
-with open("../cleaned_data.json") as f:
+with open("./cleaned_data.json") as f:
     data = json.load(f)
 '''
 Ferry/positioning, Domestic Scheduled Passenger, Military, Ambulance
@@ -9,39 +9,115 @@ International Scheduled Passenger, Int\'l Non Scheduled Passenger,
  Parachuting. Survey/research, Fire fighting,  Agricultural
  '''
 services = ['Ambulance','Executive','Survey/research','Fire fighting']
-passenger = ['Domestic Scheduled Passenger','Domestic Non Scheduled Passenger','International Scheduled Passenger','Int\'l Non Scheduled Passenger',' Passenger']
+passengers = ['Domestic Scheduled Passenger','Domestic Non Scheduled Passenger','International Scheduled Passenger','Int\'l Non Scheduled Passenger',' Passenger']
 military = ['Military','Official state flight']
 cargo = ['Cargo']
-misc = ['Private','Parachuting','Illegal Flight','Ferry/positioning','Test','Training']
+misc = ['Private','Parachuting','Illegal Flight','Ferry/positioning','Test','Training','Unknown']
 
 li = list(data.keys())
-sdic = []
-pdic = []
-mdic = []
-cdic = []
-idic = []
+service_data = []
+passenger_data = []
+military_data = []
+cargo_data = []
+misc_data = []
+
+
+
+fin = {}
+fin["name"]="All crashes"
+fin["children"]=[]
+
+
 
 for i in range(len(li)):
     k = li[i]
     n = data[k]["Nature:"]
     if n in services:
-        sdic.append(k)
-    elif n in passenger:
-        pdic.append(k)
+        service_data.append(data[k])
+    elif n in passengers:
+        passenger_data.append(data[k])
     elif n in military:
-        mdic.append(k)
+        military_data.append(data[k])
     elif n in cargo:
-        cdic.append(k)
+        cargo_data.append(data[k])
     elif n in misc:
-        idic.append(k)
+        misc_data.append(data[k])
 
 
-print(len(sdic))
-print(len(pdic))
-print(len(mdic))
-print(len(cdic))
-print(len(idic))
+print(len(service_data))
+print(len(passenger_data))
+print(len(military_data))
+print(len(cargo_data))
+print(len(misc_data))
 
+servdict={}
+mildict={}
+miscdict={}
+
+passendict={}
+# print(servdict.keys())
+
+
+
+def layer1(fl_data,fldict):
+    for i in fl_data:
+        # print(i)
+        if i["Nature:"] in fldict.keys():
+            fldict[i["Nature:"]]+=1
+        else:
+            fldict[i["Nature:"]] = 1
+    print(fldict)
+
+layer1(service_data, servdict)
+layer1(military_data, mildict)
+layer1(misc_data, miscdict)
+
+layer1(passenger_data, passendict)
+
+
+def addlayer1(fldict,name):
+    newdict={}
+    newdict['name']=name
+    newdict['children']=[]
+    for i in fldict:
+        print(i,"lololol")
+        temp={}
+        temp['name'] = i 
+        temp['size'] = fldict[i]
+        newdict['children'].append(temp)
+    print(newdict)
+    fin['children'].append(newdict)
+    # print(i, "   ", miscdict[i])
+
+
+addlayer1(servdict,"Service")
+addlayer1(mildict,"Military")
+addlayer1(miscdict,"Miscelaneous")
+
+addlayer1(passendict,"PAssenger")
+
+def addlayer0(fl_data,name):
+    newdict={}
+    newdict['name']=name
+    newdict['size']=len(fl_data)
+    fin['children'].append(newdict)
+
+addlayer0(cargo_data,"Cargo")
+
+
+# fin['children'].append
+print(servdict)
+print("---------------------")
+print(fin)
+# print(fin['children'][0]['children'])
+
+
+
+
+
+
+
+"""
 ## FINAL JSON ###
 anna = {}
 anna["name"] = "flare"
@@ -67,5 +143,6 @@ anna["children"] = [
             "children": []
         }
 ]
-for i in range(len(sdic)):
+for i in range(len(service)):
     val = {"name"}
+"""
